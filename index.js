8e2b7aca3e177bd0a64e7d4e24142fc0867c8eeb
Egg-Egg-Egg-scrambked <134942188+Egg-Egg-Egg-scrambked@users.noreply.github.com>
2026-08-01 02:47:48 +0900

    index.js

--- index.js
+++ index.js
@@ -36,3 +36,14 @@ client.on('guildMemberRemove', async (member) => {
 
 client.login(process.env.TOKEN);
 
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
